b32ec7edbe6ec93801a3cbacc3c88d24f2d15e41
samho <samho@chromium.org>
2025-09-17 20:35:54 +0200

    Initialisation and skeleton code.

---
+++ Game.java
@@ -0,0 +1,111 @@
+public class Game {
+    //Static values
+    static int gameWidth = 3000;
+    static int gameHeight = 1600;
+    static int blockerWidth = 100;
+    static int blockerHeight = 500;
+    static int ballWidth = 100;
+    static int ballHeight = 100;
+    static int LBlockerX = 200;
+    static int RBlockerX = gameWidth-(LBlockerX+blockerWidth);
+    static int FPS = 60;
+
+    // Constant attributes
+    private boolean leftAI; // If the left blocker is played by AI.
+    private boolean rightAI; // If the right blocker is played by AI.
+    private int gameSpeed = 1;
+    private int ballAmount; // Allow multiple balls in play.
+
+    // Changing attributes
+    // All position coordinates are the bottom left corner of an object.
+    private float ballSpeed;
+    private float[] ballPos; // {ball1X,ball1Y,ball2X,ball2Y,...}
+    private float[] ballVel; // ...
+    private float LBlockerPos;
+    private float LBlockerVel;
+    private float RBlockerPos;
+    private float RBlockerVel;
+    private int[] ballScore; // Amount of balls scored for L & R. {LScore,RScore}. 
+    private boolean gameOver;
+    private char winner;
+
+
+    public Game(boolean LAI,boolean RAI,int speed,int bAmount) {
+        leftAI = LAI;
+        rightAI = RAI;
+        gameSpeed = speed;
+        ballAmount = bAmount;
+
+        ballSpeed = 10;
+        ballPos = new float[2*ballAmount];
+        ballVel = new float[2*ballAmount];
+        ballScore = new int[2];
+        gameOver = false;
+    }
+    public void Start() {
+        // Initialise ballPositions.
+        for (int i=0;i<ballAmount;i++) {
+            if (i % 2 == 0) 
+                ballPos[i] = (gameWidth-ballWidth)/2; // Middle of the ball is in the middle of the screen.
+            else
+                ballPos[i] = (gameHeight-ballHeight)/2; // Middle of the ball is in the middle of the screen.
+        }
+        
+        // Initialise ballVelocities.
+        // Find a random avarage direction angle for all balls.
+        double randAng;
+        if (Math.random() < 0.5) { // Randomly chooses direction to the left.
+            randAng = 140*Math.random()+110; // ang:(110,250)
+        }
+        else {
+            randAng = 140*(Math.random()-0.5); // ang:(-70,70)
+        }
+        // Make random directions based on the avarage angle.
+        double ang;
+        for (int i=0;i<ballAmount;i=i+2) {
+            ang = randAng + 10*(Math.random()-0.5);
+            ballVel[i] = (float)Math.cos(ang)*ballSpeed;
+            ballVel[i+1] = (float)Math.sin(ang)*ballSpeed;
+        }
+
+        // Initialise blockers.
+        LBlockerPos = (gameHeight-blockerHeight)/2;
+        LBlockerVel = 0;
+        RBlockerPos = (gameHeight-blockerHeight)/2;
+        RBlockerVel = 0;
+
+        // Run the game.
+        Run();
+    }
+    public void Run() {
+
+        while (!gameOver) {
+            sendUpdate();
+            RunPhysics();
+        }
+
+        sendUpdate();
+        GameOverUpdate();
+    }
+    public void RunPhysics() {
+        return;
+    }
+    public void Listen(String actionL,String actionR) {
+        return;
+    }
+    public float[] sendUpdate() {
+        // {LeftBlockerY,RightBlockerY,Ball1X,Ball1Y,Ball2X,ball2Y,...}
+        float[] update = new float[ballPos.length+2];
+        update[0] = LBlockerPos; update[1] = RBlockerPos;
+        for(int i=0;i<ballAmount*2;i++) {
+            update[i+2] = ballPos[i];
+        }
+        return update;
+    }
+    public String GameOverUpdate() {
+        if (winner == 'L') {
+            return "W->L:" + Integer.toString(ballScore[0]) + "-" + Integer.toString(ballScore[1]);
+        }
+        return "W->R:" + Integer.toString(ballScore[1]) + "-" + Integer.toString(ballScore[0]);
+    }
+}
--- README.md
+++ README.md
@@ -6,4 +6,6 @@ A simple pong game which is intended to have a simple AI framework.
 * Multiple balls
 
 ## Parameters
-Gamesize ratio (15x8)
+* Gamesize ratio (30x16)
+* Blocker ratio (1x5)
+* Ball ratio (1x1)
